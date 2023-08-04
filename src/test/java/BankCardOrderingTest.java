9bebb15d6af5f77827fd2f1b4a08d719c149d85d
Vladko1973 <vpk1973@yandex.ru>
2023-08-04 14:15:44 +0300

    third commit

--- src/test/java/BankCardOrderingTest.java
+++ src/test/java/BankCardOrderingTest.java
@@ -7,11 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -47,7 +44,7 @@ public class BankCardOrderingTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79630000000");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        var textActual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String textActual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", textActual);
     }
 }
